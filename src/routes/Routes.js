dba14652daeb1374382ab9b5b28025abb45d31e4
Arup Gorai <arupgorai.js@gmail.com>
2020-11-27 02:44:25 +0530

    initial route page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../screens/Login';
+import ProductList from '../screens/ProductList';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function AppRouter() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Products" component={ProductList} />
         </Stack.Navigator>
       </NavigationContainer>
     );
